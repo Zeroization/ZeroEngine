9e0929f51732ab518a5d0d81e88119a747b14603
Zeroization <1255108389@qq.com>
2025-03-11 16:24:42 +0800

    [feature] Runtime模块添加断言

---
+++ Engine/Source/Runtime/Core/Assert/Assert.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+
+#include "Core/Log/Logger.h"
+
+// debugbreak()
+#ifdef ZERO_OS_WINDOWS
+    #define ZERO_DEBUG_BREAK __debugbreak()
+#elif defined(ZERO_COMPILER_CLANG)
+    #define ZERO_DEBUG_BREAK __builtin_debugtrap()
+#else
+    #defineZERO_DEBUG_BREAK
+#endif
+
+// Assertions
+#ifdef ZERO_DEBUG_ENABLE
+    #define ZERO_ASSERTS_ENABLE
+#endif
+
+#ifdef ZERO_ASSERTS_ENABLE
+    #define ZERO_CORE_ASSERT_MESSAGE_INTERNAL(message) LOG_CRITICAL(std::format("[{}] Core Assertion Failed: {}", __FUNCTION__, message))
+    #define ZERO_ASSERT_MESSAGE_INTERNAL(message) LOG_CRITICAL(std::format("[{}] Assertion Failed: {}", __FUNCTION__, message))
+    #define ZERO_CORE_ASSERT(condition, message) { if(!(condition)) { ZERO_CORE_ASSERT_MESSAGE_INTERNAL(message); ZERO_DEBUG_BREAK; } }
+    #define ZERO_ASSERT(condition, message) { if(!(condition)) { ZERO_ASSERT_MESSAGE_INTERNAL(message); ZERO_DEBUG_BREAK; } }
+#else
+    #define ZERO_CORE_ASSERT(condition, message)
+    #define ZERO_ASSERT(condition, message)
+#endif
--- Engine/Source/Runtime/pch.h
+++ Engine/Source/Runtime/pch.h
@@ -6,19 +6,19 @@
 // ZERO_OS_WINDOWS: Windows系统
 // ZERO_OS_LINUX:   Linux系统     TODO
 #if defined(_WIN32) || defined(_WIN64)
-#define ZERO_OS_WINDOWS
+    #define ZERO_OS_WINDOWS
 #elif defined(__linux__)
-#define ZERO_OS_LINUX
+    #define ZERO_OS_LINUX
 #else
-#error "Pch Error: Unsupport OS!!!!!"
+    #error "Pch Error: Unsupport OS!!!!!"
 #endif
 
 // 平台定义=============================
 // ZERO_PLATFORM_DESKTOP: 桌面端
 #if defined(ZERO_OS_WINDOWS) || defined(ZERO_OS_LINUX)
-#define ZERO_PLATFORM_DESKTOP
+    #define ZERO_PLATFORM_DESKTOP
 #else
-#error "Pch Error: Unsupport platform!!!!!"
+    #error "Pch Error: Unsupport platform!!!!!"
 #endif
 
 // 图形API定义=========================
@@ -26,24 +26,46 @@
 // 1: Vulkan    TODO
 // 2: D3D12     TODO
 #if defined(ZERO_PLATFORM_DESKTOP)
-#define ZERO_GRAPHIC_SELECTOR 0
+    #define ZERO_GRAPHIC_SELECTOR 0
 #endif
 
 #if ZERO_GRAPHIC_SELECTOR == 0
-#define ZERO_GRAPHIC_OPENGL
+    #define ZERO_GRAPHIC_OPENGL
 #elif ZERO_GRAPHIC_SELECTOR == 1
-#define ZERO_GRAPHIC_VULKAN
+    #define ZERO_GRAPHIC_VULKAN
 #elif ZERO_GRAPHIC_SELECTOR == 2
-#define ZERO_GRAPHIC_D3D12
+    #define ZERO_GRAPHIC_D3D12
 #else
-#error "Pch Error: Unknown Graphic API selector"
+    #error "Pch Error: Unknown Graphic API selector"
 #endif
 
 // 图形API兼容性检查=====================
 // Windows系统:   OpenGL, Vulkan, D3D12
 // Linux系统:     OpenGL, Vulkan
 #if defined(ZERO_OS_LINUX) && defined(ZERO_GRAPHIC_D3D12)
-#error "Pch Error: Linux OS & D3D12 Not Supported!!!!!"
+    #error "Pch Error: Linux OS & D3D12 Not Supported!!!!!"
+#endif
+
+// 编译器检测===========================
+#if defined(__GNUC__)
+    #if defined(__clang__)
+        #define ZERO_COMPILER_CLANG
+    #else
+        #define ZERO_COMPILER_GCC
+    #endif
+#elif defined(_MSC_VER)
+    #define ZERO_COMPILER_MSVC
+#endif
+
+#ifdef ZERO_COMPILER_MSVC
+    #define ZERO_FORCE_INLINE __forceinline
+    #define ZERO_EXPLICIT_STATIC static
+#elif defined(__GNUC__)
+    #define ZERO_FORCE_INLINE __attribute((always_inline)) inline
+    #define ZERO_EXPLICIT_STATIC
+#else
+    #define ZERO_FORCE_INLINE inline
+    #define ZERO_EXPLICIT_STATIC
 #endif
 #pragma endregion
 
@@ -59,4 +81,4 @@
 
 // 自己的库===========================
 #include "Function/Input/InputDefines.h"
-#include "Core/Log/Logger.h"
+#include "Core/Assert/Assert.h"
