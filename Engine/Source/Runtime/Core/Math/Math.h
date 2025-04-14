d7eb63c047941ca86996b31b404cb57b05178c38
Zeroization <1255108389@qq.com>
2025-04-14 14:59:24 +0800

    [feature] 添加GLM数学库

---
+++ Engine/Source/Runtime/Core/Math/Math.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+#include "pch.h"
+
+#if defined(ZERO_GRAPHIC_OPENGL) || defined(ZERO_GRAPHIC_VULKAN)
+#include <glm/glm.hpp>
+#elif defined(ZERO_GRAPHIC_D3D12)
+// FIXME: 此预定义未验证
+#define GLM_FORCE_LEFT_HANDED
+#define GLM_FORCE_MATRIX_ROW_MAJOR
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/glm.hpp>
+#endif
